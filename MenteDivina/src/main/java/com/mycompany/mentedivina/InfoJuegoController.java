f35770cb5aaf758caf8578a497fc4b95dde4ca86
kevinMaga <kerimaga@espol.edu.ec>
2024-08-10 15:25:18 -0500

    Ventana info

--- MenteDivina/src/main/java/com/mycompany/mentedivina/InfoJuegoController.java
+++ MenteDivina/src/main/java/com/mycompany/mentedivina/InfoJuegoController.java
@@ -10,9 +10,11 @@ import java.net.URL;
 import java.util.ResourceBundle;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
+import javafx.scene.text.Font;
 import modelo.Juego;
 
 /**
@@ -32,8 +34,16 @@ public class InfoJuegoController implements Initializable {
     private Label nombreJuego;
     
     public static Juego juego;
+    @FXML
+    private Label LBLDescripcion;
+    @FXML
+    private Button BTNRegresar;
+    @FXML
+    private Button BTNSeguir;
     @Override
     public void initialize(URL url, ResourceBundle rb) {
+        
+        Font font3 = Font.loadFont(getClass().getResourceAsStream("/fonts/LuckiestGuy-Regular.ttf"), 18);
         try (FileInputStream fis = new FileInputStream(juego.getRutaImagen())) {
             Image image = new Image(fis, 250, 250, true, true);
             imagenJuego.setImage(image);
@@ -41,6 +51,10 @@ public class InfoJuegoController implements Initializable {
             e.printStackTrace();
         }
         nombreJuego.setText(juego.getNombre());
+        LBLDescripcion.setFont(font3);
+        BTNRegresar.setFont(font3);
+        BTNSeguir.setFont(font3);
     }
+    
     
 }
